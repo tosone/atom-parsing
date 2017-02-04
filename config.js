21016f0c584722ae4519b8765ae898ff06ff505e
tosone <i@tosiney.com>
2017-02-04 15:53:47 +0800

    update for new version

--- config.js
+++ config.js
@@ -7,16 +7,15 @@ const mqtt = require('mqtt');
 const pinyin = require('convertPinyin');
 
 const host = '120.27.94.189';
+const password = '8541539655';
 
-const redis1 = redis.createClient({ 'db': 1, host });
-const redis3 = redis.createClient({ 'db': 3, host });
-const redis8 = redis.createClient({ 'db': 8, host });
+const redis1 = redis.createClient({ 'db': 1, host, password });
+const redis3 = redis.createClient({ 'db': 3, host, password });
+const redis8 = redis.createClient({ 'db': 8, host, password });
 
 let mqttClient = mqtt.connect('mqtt://' + host + ':1883');
 
-mqttClient.on('connect', () => {
-  mqttClient.subscribe('#');
-});
+mqttClient.on('connect', () => { mqttClient.subscribe('#'); });
 
 const macLocation = "/sys/class/net/wlan0/address";
 let macAddress = null;
