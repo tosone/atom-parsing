8fc0d826aa3e30d939e2de8bdb316333f56a6f3b
tosone <i@tosiney.com>
2017-04-05 11:48:28 +0800

    Need to set mongodb url in your path. Will read `MONGODBURL` from env.
    Signed-off-by: tosone <i@tosiney.com>

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Press `alt+x` review `manifest.rc` file for test.
 
 ![screenshot](https://github.com/tosone/atom-parsing/raw/master/demo.gif)
+
+## New Version
+
+- Need to set mongodb url in your path.
--- config.js
+++ config.js
@@ -1,24 +1,29 @@
-'use strict';
-
 const fs = require('fs');
-
 const redis = require('redis');
 const mqtt = require('mqtt');
 const pinyin = require('convertPinyin');
-
 const host = '120.27.94.189';
 const password = '8541539655';
-
 const redis1 = redis.createClient({ 'db': 1, host, password });
 const redis3 = redis.createClient({ 'db': 3, host, password });
 const redis8 = redis.createClient({ 'db': 8, host, password });
+const mosca = require('mosca');
 
-let mqttClient = mqtt.connect('mqtt://' + host + ':1883');
+new mosca.Server({
+  port: 12721,
+  backend: {
+    type: 'mongo',
+    url: process.env.MONGODBURL ? process.env.MONGODBURL : 'mongodb://localhost:27017/test',
+    pubsubCollection: 'parsing',
+    mongo: {}
+  }
+});
 
+let mqttClient = mqtt.connect('mqtt://127.0.0.1:12721');
 mqttClient.on('connect', () => { mqttClient.subscribe('#'); });
 
-const macLocation = "/sys/class/net/wlan0/address";
-let macAddress = null;
+const macLocation = '';
+let macAddress = '';
 if (fs.existsSync(macLocation)) {
   macAddress = fs.readFileSync(macLocation).toString().trim().toUpperCase();
 }
